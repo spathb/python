2a3ce4215fbe55b538667c8cd94f45a198b46ac9
spathb <bs12_22@hotmail.com>
2017-02-19 14:04:19 -0800

    Update drill 2.py

--- drill 2.py
+++ drill 2.py
@@ -1,26 +1,8 @@
-import os
-    
-old = "test1/doc1.txt"
-new = "test2/doc1.txt"
+import glob
+import shutil
+dest_dir = "C:/Users/Bret Spath/Desktop/b"
+for file in glob.glob(r'C:/Users/Bret Spath/Desktop/a/*.txt'):
+    print (file)                                                                                                                                        
+    shutil.copy(file, dest_dir)
 
-os.rename(old,new)
-print(os.path.abspath(new))
-
-old = "test1/doc2.txt"
-new = "test2/doc2.txt"
-
-os.rename(old,new)
-print(os.path.abspath(new))
-
-old = "test1/doc3.txt"
-new = "test2/doc3.txt"
-
-os.rename(old,new)
-print(os.path.abspath(new))
-
-old = "test1/doc4.txt"
-new = "test2/doc4.txt"
-
-os.rename(old,new)
-print(os.path.abspath(new))
-
+shutil.rmtree('C:/Users/Bret Spath/Desktop/a')
